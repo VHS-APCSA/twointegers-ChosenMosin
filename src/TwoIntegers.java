9cf29f8336309b4e9d3c51e7e78022cd56ed9c5c
bob <bob@APCSA>
2017-09-18 14:51:05 -0700

    Created arithmatic class along with get and set methods

---
+++ src/TwoIntegers.java
@@ -0,0 +1,28 @@
+
+public class TwoIntegers 
+{
+	private int num1;
+	private int num2;
+	
+	public TwoIntegers()
+	{
+		num1 = 4;
+		num2 = 3;
+	}
+	public int getNum1() 
+	{
+		return num1;
+	}
+	public void setNum1(int num1) 
+	{
+		this.num1 = num1;
+	}
+	public int getNum2() 
+	{
+		return num2;
+	}
+	public void setNum2(int num2) 
+	{
+		this.num2 = num2;
+	}
+}
