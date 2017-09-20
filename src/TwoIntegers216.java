c171efb86210708d3c5d2448e05841d5e6b39cc0
bob <bob@APCSA>
2017-09-20 14:05:36 -0700

    Started on 2.16 of arithmetic

---
+++ src/TwoIntegers216.java
@@ -0,0 +1,16 @@
+
+public class TwoIntegers216 
+{
+	private int num1;
+	private int num2;
+	
+	public TwoIntegers216()
+	{
+		num1 = 0;
+		num2 = 0;
+	}
+	public TwoIntegers216(int num1, int num2)
+	{
+		
+	}
+}
