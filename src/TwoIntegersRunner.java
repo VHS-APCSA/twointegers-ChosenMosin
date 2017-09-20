e63244cd3af0c4ebd5b858dfa9719e59ec603dda
bob <bob@APCSA>
2017-09-20 13:46:47 -0700

    Slight tweaks in runner to help format, but nothing too serious.

--- src/TwoIntegersRunner.java
+++ src/TwoIntegersRunner.java
@@ -2,16 +2,18 @@ import java.util.Scanner;
 
 public class TwoIntegersRunner 
 {
+	private static Scanner input;
+
 	public static void main(String[] args)
 	{
-		Scanner input = new Scanner(System.in);
+		input = new Scanner(System.in);
 		int num1;
 		int num2;
 		int sum;
 		
-		System.out.print("Enter first integer");
+		System.out.print("Enter first integer \n");
 		num1 = input.nextInt();
-		System.out.print("Enter second integer");
+		System.out.print("Enter second integer \n");
 		num2 = input.nextInt();
 		
 		sum = num1 + num2;
